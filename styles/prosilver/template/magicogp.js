986c30a4bd0a7416de59e1491ad3f8a83595336c
Ger Bruinsma <gerbruinsma@ktba.com>
2017-09-15 09:44:58 +0200

    Pass without image

--- styles/prosilver/template/magicogp.js
+++ styles/prosilver/template/magicogp.js
@@ -2,7 +2,11 @@
     $('.content .postlink').each(function() {
         var ogpdata = $(this).data('ogp');
         if (ogpdata.title !== undefined) {
-            var block = '<div class="ogpblock"><img src="' + ogpdata.image + '"/><div class="ogptext"><h4>' + ogpdata.title + '</h4>' + ogpdata.description + '</div></div>';
+            var block = '<div class="ogpblock">';
+            if (ogpdata.image !== undefined) {
+                block += '<img src="' + ogpdata.image + '"/>';
+            }
+            block += '<div class="ogptext"><h4>' + ogpdata.title + '</h4>' + ogpdata.description + '</div></div>';
             $(this).html(block);
             $(this).addClass('ogplink');
             $(this).removeAttr('data-ogp');
