7fcc936304d5981f9bc56b3be5582849a20e470f
Ger Bruinsma <ger_bruinsma@hotmail.com>
2018-05-15 14:02:57 +0200

    mChat compat

--- styles/prosilver/template/magicogp.js
+++ styles/prosilver/template/magicogp.js
@@ -1,15 +1,28 @@
 (function ($) {
-    $('.content .postlink').each(function() {
-        var ogpdata = $(this).data('ogp');
-        if (ogpdata.title !== undefined) {
-            var block = '<div class="ogpblock">';
-            if (ogpdata.image !== undefined) {
-                block += '<img src="' + ogpdata.image + '"/>';
+    "use strict";
+
+function parseOgp($container) {
+        $container.find('.postlink').each(function() {
+            var ogpdata = $(this).data('ogp');
+            if (ogpdata.title !== undefined) {
+                var block = '<div class="ogpblock">';
+                if (ogpdata.image !== undefined) {
+                    block += '<img src="' + ogpdata.image + '"/>';
+                }
+                block += '<div class="ogptext"><h4>' + ogpdata.title + '</h4>' + ogpdata.description + '</div></div>';
+                $(this).html(block);
+                $(this).addClass('ogplink');
+                $(this).removeAttr('data-ogp');
             }
-            block += '<div class="ogptext"><h4>' + ogpdata.title + '</h4>' + ogpdata.description + '</div></div>';
-            $(this).html(block);
-            $(this).addClass('ogplink');
-            $(this).removeAttr('data-ogp');
-        }
-    });
+        });
+    }
+
+    parseOgp($('.content'));
+
+    if (typeof mChat === 'object') {
+        $(mChat).on('mchat_add_message_before', function(e, data) {
+            parseOgp(data.message);
+        });
+    }
+
 })(jQuery);
